fa6947cbe76c437eaddb904c8fc693e1827fadcd
SergiyMi <bladeneo@i.ua>
2019-12-01 18:50:05 +0200

    Updates

---
+++ precache-manifest.338cf1b4d6eec2eefa1d26012f5cfd70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc4e08196ecb2bbada1617c6956e72c5",
+    "url": "/goit-react-hw-02-image-finder/index.html"
+  },
+  {
+    "revision": "012aba2e1a55e19c12fc",
+    "url": "/goit-react-hw-02-image-finder/static/css/2.5a9933e8.chunk.css"
+  },
+  {
+    "revision": "76818f7e603655b35fce",
+    "url": "/goit-react-hw-02-image-finder/static/css/main.b92a17e3.chunk.css"
+  },
+  {
+    "revision": "012aba2e1a55e19c12fc",
+    "url": "/goit-react-hw-02-image-finder/static/js/2.23c9a414.chunk.js"
+  },
+  {
+    "revision": "76818f7e603655b35fce",
+    "url": "/goit-react-hw-02-image-finder/static/js/main.4d408bf2.chunk.js"
+  },
+  {
+    "revision": "ac7b7b3bbc51153f2ab0",
+    "url": "/goit-react-hw-02-image-finder/static/js/runtime-main.cf447ae0.js"
+  }
+]);
