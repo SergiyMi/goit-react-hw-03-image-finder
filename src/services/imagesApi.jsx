d7fe2aa61fef54bd496134e4723e983a167f0fbf
SergiyMi <bladeneo@i.ua>
2019-12-04 13:19:53 +0200

    use axios instead fetch and media in css

--- src/services/imagesApi.jsx
+++ src/services/imagesApi.jsx
@@ -1,9 +1,11 @@
+import axios from 'axios';
+
 const URL = 'https://pixabay.com/api/';
 const KEY = 'key=14462661-6a591cd8dedfbd6803de54cb4';
 const PER_PAGE = 'per_page=12';
 
 export const fetchImages = (query = '', pageNumber = 0) => {
-  return fetch(`${URL}?${KEY}&q=${query}&page=${pageNumber}&${PER_PAGE}`)
-    .then(res => res.json())
-    .then(data => data.hits);
+  return axios
+    .get(`${URL}?${KEY}&q=${query}&page=${pageNumber}&${PER_PAGE}`)
+    .then(data => data.data.hits);
 };
